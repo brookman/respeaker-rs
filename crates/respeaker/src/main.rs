d196c973843db8efa213e89835c5093d4aed39f9
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2025-04-07 18:40:39 +0200

    feat: implement connect, read and list

--- crates/respeaker/src/main.rs
+++ crates/respeaker/src/main.rs
@@ -1,8 +1,21 @@
 use clap::{Parser, Subcommand, command};
+use eyre::Ok;
 use eyre::Result;
+use eyre::bail;
 use eyre::eyre;
+use params::Access;
+use params::Param;
+use params::ParamConfig;
+use rusb::DeviceHandle;
+use rusb::GlobalContext;
+use std::time::Duration;
+use strum::IntoEnumIterator;
+use tabled::Table;
+use tabled::Tabled;
 use tracing::Level;
 use tracing::info;
+
+mod params;
 
 /// Unofficial CLI & UI for the ReSpeaker Mic Array v2.0
 #[derive(Parser, Debug)]
@@ -10,19 +23,45 @@ use tracing::info;
 struct Arguments {
     #[command(subcommand)]
     command: Option<Command>,
+
+    #[clap(short = 'i')]
+    device_index: Option<usize>,
 }
 
 #[derive(Subcommand, Debug)]
 #[clap(flatten_help = true)]
 enum Command {
-    /// List the available config parameters and their values
+    /// List all available parameters and their current values (RW and RO)
     List,
+    /// Read the value of a specific parameter
+    Read { param: Param },
+    /// Write the value of a specific parameter
+    Write { param: Param, value: String },
 }
 
 fn main() -> eyre::Result<()> {
     let args: Arguments = init()?;
 
     info!("Running unofficial ReSpeaker CLI with {args:?}");
+
+    let device = open_device(args.device_index)?;
+
+    if let Some(command) = args.command {
+        match command {
+            Command::List => {
+                let list = list(&device)?;
+                info!("Parameters:\n{list}");
+            }
+            Command::Read { param } => {
+                let config = param.config();
+                let value = read(&device, config)?;
+                info!("\n{param:?}={value}");
+            }
+            Command::Write { param, value } => todo!(),
+        }
+    } else {
+        info!("Opening UI...");
+    }
 
     Ok(())
 }
@@ -38,4 +77,145 @@ where
         .try_init()
         .map_err(|e| eyre!("Tracing init error: {e}"))?;
     Ok(args)
+}
+
+fn open_device(device_index: Option<usize>) -> Result<DeviceHandle<GlobalContext>> {
+    const VENDOR_ID: u16 = 0x2886;
+    const PRODUCT_ID: u16 = 0x0018;
+
+    info!("Searching for ReSpeaker Mic Array v2.0 device...");
+
+    let mut devices = vec![];
+
+    for device in rusb::devices()?.iter() {
+        let device_desc = device.device_descriptor()?;
+
+        if device_desc.vendor_id() == VENDOR_ID && device_desc.product_id() == PRODUCT_ID {
+            info!(
+                "Found: Bus {:03} Device {:03} ID {:04x}:{:04x}",
+                device.bus_number(),
+                device.address(),
+                device_desc.vendor_id(),
+                device_desc.product_id()
+            );
+            devices.push(device);
+        }
+    }
+    if let Some(i) = device_index {
+        if let Some(d) = devices.get(i) {
+            return Ok(d.open()?);
+        }
+        bail!(
+            "Device index (-i argument) out of range. Index was {i} but {} devices found.",
+            devices.len()
+        );
+    }
+    if devices.len() == 1 {
+        let handle = devices[0].open()?;
+        return Ok(handle);
+    }
+    if devices.len() > 1 {
+        bail!("Multiple devices found. Specify the a device index with -i.")
+    }
+
+    bail!("No devices found")
+}
+
+#[derive(Tabled)]
+struct TableRow {
+    name: String,
+    value: String,
+    t: String,
+    access: String,
+    range: String,
+    description: String,
+    values: String,
+}
+
+fn list(device_handle: &DeviceHandle<GlobalContext>) -> Result<String> {
+    let mut rows = vec![];
+    for p in Param::iter() {
+        let config = p.config();
+        let value = read(device_handle, config)?;
+        match config {
+            ParamConfig::IntN(config)
+            | ParamConfig::Int2(config)
+            | ParamConfig::Int3(config)
+            | ParamConfig::Int4(config) => rows.push(TableRow {
+                name: format!("{p:?}"),
+                value,
+                t: "int".to_string(),
+                access: if config.access == Access::ReadOnly {
+                    "ro"
+                } else {
+                    "rw"
+                }
+                .to_string(),
+                range: format!("{}..{}", config.min, config.max),
+                description: config.description.clone(),
+                values: config.value_descriptions.join("\n"),
+            }),
+            ParamConfig::Float(config) => rows.push(TableRow {
+                name: format!("{p:?}"),
+                value,
+                t: "float".to_string(),
+                access: if config.access == Access::ReadOnly {
+                    "ro"
+                } else {
+                    "rw"
+                }
+                .to_string(),
+                range: format!("{}..{}", config.min, config.max),
+                description: config.description.clone(),
+                values: config.value_descriptions.join("\n"),
+            }),
+        }
+    }
+    Ok(Table::new(rows).to_string())
+}
+
+fn read(device_handle: &DeviceHandle<GlobalContext>, param_config: &ParamConfig) -> Result<String> {
+    let (is_int, id, cmd) = match param_config {
+        ParamConfig::IntN(config)
+        | ParamConfig::Int2(config)
+        | ParamConfig::Int3(config)
+        | ParamConfig::Int4(config) => (true, config.id, config.cmd),
+        ParamConfig::Float(config) => (false, config.id, config.cmd),
+    };
+
+    let mut cmd = 0x80 | cmd;
+    if is_int {
+        cmd |= 0x40;
+    }
+
+    let mut buffer = [0u8; 8];
+
+    let request_type = rusb::request_type(
+        rusb::Direction::In,
+        rusb::RequestType::Vendor,
+        rusb::Recipient::Device,
+    );
+
+    device_handle.read_control(
+        request_type,
+        0,
+        cmd,
+        id,
+        &mut buffer,
+        Duration::from_secs(5),
+    )?;
+    let response = (
+        i32::from_le_bytes(buffer[0..4].try_into()?),
+        i32::from_le_bytes(buffer[4..8].try_into()?),
+    );
+
+    let result = if is_int {
+        format!("{}", response.0)
+    } else {
+        #[allow(clippy::cast_possible_truncation)]
+        let float = (f64::from(response.0) * f64::from(response.1).exp2()) as f32;
+        format!("{float}")
+    };
+
+    Ok(result)
 }
---
+++ crates/respeaker/src/params.rs
@@ -0,0 +1,240 @@
+use std::sync::OnceLock;
+
+use clap::ValueEnum;
+use enum_map::{Enum, EnumMap, enum_map};
+use strum_macros::EnumIter;
+
+#[allow(clippy::upper_case_acronyms)] // ReSpeaker API uses UPPERCASE
+#[allow(non_camel_case_types)] // ReSpeaker API uses UPPERCASE
+#[derive(Clone, Debug, Enum, ValueEnum, EnumIter)]
+#[clap(rename_all = "verbatim")]
+pub enum Param {
+    AECFREEZEONOFF,
+    AECNORM,
+    AECPATHCHANGE,
+    AECSILENCELEVEL,
+    AECSILENCEMODE,
+    AGCDESIREDLEVEL,
+    AGCGAIN,
+    AGCMAXGAIN,
+    AGCONOFF,
+    AGCTIME,
+    CNIONOFF,
+    DOAANGLE,
+    ECHOONOFF,
+    FREEZEONOFF,
+    FSBPATHCHANGE,
+    FSBUPDATED,
+    GAMMAVAD_SR,
+    GAMMA_E,
+    GAMMA_ENL,
+    GAMMA_ETAIL,
+    GAMMA_NN,
+    GAMMA_NN_SR,
+    GAMMA_NS,
+    GAMMA_NS_SR,
+    HPFONOFF,
+    MIN_NN,
+    MIN_NN_SR,
+    MIN_NS,
+    MIN_NS_SR,
+    NLAEC_MODE,
+    NLATTENONOFF,
+    NONSTATNOISEONOFF,
+    NONSTATNOISEONOFF_SR,
+    RT60,
+    RT60ONOFF,
+    SPEECHDETECTED,
+    STATNOISEONOFF,
+    STATNOISEONOFF_SR,
+    TRANSIENTONOFF,
+    VOICEACTIVITY,
+}
+
+impl Param {
+    pub fn config(&self) -> &ParamConfig {
+        static MAP: OnceLock<EnumMap<Param, ParamConfig>> = OnceLock::new();
+        &MAP.get_or_init(|| {
+            enum_map! {
+                Self::AECFREEZEONOFF => ParamConfig::int2(18, 7, 1, 0, Access::ReadWrite, "Adaptive Echo Canceler updates inhibit.", "0 = Adaptation enabled", "1 = Freeze adaptation, filter only"),
+                Self::AECNORM => ParamConfig::float(18, 19, 16., 0.25, Access::ReadWrite, "Limit on norm of AEC filter coefficients"),
+                Self::AECPATHCHANGE => ParamConfig::int2(18, 25, 1, 0, Access::ReadOnly, "AEC Path Change Detection.", "0 = false (no path change detected)", "1 = true (path change detected)"),
+                Self::RT60 => ParamConfig::float(18, 26, 0.9, 0.25, Access::ReadOnly, "Current RT60 estimate in seconds"),
+                Self::HPFONOFF => ParamConfig::int4(18, 27, 3, 0, Access::ReadWrite, "High-pass Filter on microphone signals.", "0 = OFF", "1 = ON - 70 Hz cut-off", "2 = ON - 125 Hz cut-off", "3 = ON - 180 Hz cut-off"),
+                Self::RT60ONOFF => ParamConfig::int2(18, 28, 1, 0, Access::ReadWrite, "RT60 Estimation for AES.", "0 = OFF", "1 = ON"),
+                Self::AECSILENCELEVEL => ParamConfig::float(18, 30, 1., 1e-09, Access::ReadWrite, "Threshold for signal detection in AEC [-inf .. 0] dBov (Default: -80dBov = 10log10(1x10-8))"),
+                Self::AECSILENCEMODE => ParamConfig::int2(18, 31, 1, 0, Access::ReadOnly, "AEC far-end silence detection status. ", "0 = false (signal detected) ", "1 = true (silence detected)"),
+                Self::AGCONOFF => ParamConfig::int2(19, 0, 1, 0, Access::ReadWrite, "Automatic Gain Control. ", "0 = OFF ", "1 = ON"),
+                Self::AGCMAXGAIN => ParamConfig::float(19, 1, 1000., 1., Access::ReadWrite, "Maximum AGC gain factor. [0 .. 60] dB (default 30dB = 20log10(31.6))"),
+                Self::AGCDESIREDLEVEL => ParamConfig::float(19, 2, 0.99, 1e-08, Access::ReadWrite, "Target power level of the output signal. [-inf .. 0] dBov (default: -23dBov = 10log10(0.005))"),
+                Self::AGCGAIN => ParamConfig::float(19, 3, 1000., 1., Access::ReadWrite, "Current AGC gain factor. [0 .. 60] dB (default: 0.0dB = 20log10(1.0))"),
+                Self::AGCTIME => ParamConfig::float(19, 4, 1., 0.1, Access::ReadWrite, "Ramps-up / down time-constant in seconds."),
+                Self::CNIONOFF => ParamConfig::int2(19, 5, 1, 0, Access::ReadWrite, "Comfort Noise Insertion.", "0 = OFF", "1 = ON"),
+                Self::FREEZEONOFF => ParamConfig::int2(19, 6, 1, 0, Access::ReadWrite, "Adaptive beamformer updates.", "0 = Adaptation enabled", "1 = Freeze adaptation, filter only"),
+                Self::STATNOISEONOFF => ParamConfig::int2(19, 8, 1, 0, Access::ReadWrite, "Stationary noise suppression.", "0 = OFF", "1 = ON"),
+                Self::GAMMA_NS => ParamConfig::float(19, 9, 3., 0., Access::ReadWrite, "Over-subtraction factor of stationary noise. min .. max attenuation"),
+                Self::MIN_NS => ParamConfig::float(19, 10, 1., 0., Access::ReadWrite, "Gain-floor for stationary noise suppression. [-inf .. 0] dB (default: -16dB = 20log10(0.15))"),
+                Self::NONSTATNOISEONOFF => ParamConfig::int2(19, 11, 1, 0, Access::ReadWrite, "Non-stationary noise suppression.", "0 = OFF", "1 = ON"),
+                Self::GAMMA_NN => ParamConfig::float(19, 12, 3., 0., Access::ReadWrite, "Over-subtraction factor of non- stationary noise. min .. max attenuation"),
+                Self::MIN_NN => ParamConfig::float(19, 13, 1., 0., Access::ReadWrite, "Gain-floor for non-stationary noise suppression. [-inf .. 0] dB (default: -10dB = 20log10(0.3))"),
+                Self::ECHOONOFF => ParamConfig::int2(19, 14, 1, 0, Access::ReadWrite, "Echo suppression.", "0 = OFF", "1 = ON"),
+                Self::GAMMA_E => ParamConfig::float(19, 15, 3., 0., Access::ReadWrite, "Over-subtraction factor of echo (direct and early components). min .. max attenuation"),
+                Self::GAMMA_ETAIL => ParamConfig::float(19, 16, 3., 0., Access::ReadWrite, "Over-subtraction factor of echo (tail components). min .. max attenuation"),
+                Self::GAMMA_ENL => ParamConfig::float(19, 17, 5., 0., Access::ReadWrite, "Over-subtraction factor of non-linear echo. min .. max attenuation"),
+                Self::NLATTENONOFF => ParamConfig::int2(19, 18, 1, 0, Access::ReadWrite, "Non-Linear echo attenuation.", "0 = OFF", "1 = ON"),
+                Self::NLAEC_MODE => ParamConfig::int3(19, 20, 2, 0, Access::ReadWrite, "Non-Linear AEC training mode.", "0 = OFF", "1 = ON - phase 1", "2 = ON - phase 2"),
+                Self::SPEECHDETECTED => ParamConfig::int2(19, 22, 1, 0, Access::ReadOnly, "Speech detection status.", "0 = false (no speech detected)", "1 = true (speech detected)"),
+                Self::FSBUPDATED => ParamConfig::int2(19, 23, 1, 0, Access::ReadOnly, "FSB Update Decision.", "0 = false (FSB was not updated)", "1 = true (FSB was updated)"),
+                Self::FSBPATHCHANGE => ParamConfig::int2(19, 24, 1, 0, Access::ReadOnly, "FSB Path Change Detection.", "0 = false (no path change detected)", "1 = true (path change detected)"),
+                Self::TRANSIENTONOFF => ParamConfig::int2(19, 29, 1, 0, Access::ReadWrite, "Transient echo suppression.", "0 = OFF", "1 = ON"),
+                Self::VOICEACTIVITY => ParamConfig::int2(19, 32, 1, 0, Access::ReadOnly, "VAD voice activity status.", "0 = false (no voice activity)", "1 = true (voice activity)"),
+                Self::STATNOISEONOFF_SR => ParamConfig::int2(19, 33, 1, 0, Access::ReadWrite, "Stationary noise suppression for ASR.", "0 = OFF", "1 = ON"),
+                Self::NONSTATNOISEONOFF_SR => ParamConfig::int2(19, 34, 1, 0, Access::ReadWrite, "Non-stationary noise suppression for ASR.", "0 = OFF", "1 = ON"),
+                Self::GAMMA_NS_SR => ParamConfig::float(19, 35, 3., 0., Access::ReadWrite, "Over-subtraction factor of stationary noise for ASR. [0.0 .. 3.0] (default: 1.0)"),
+                Self::GAMMA_NN_SR => ParamConfig::float(19, 36, 3., 0., Access::ReadWrite, "Over-subtraction factor of non-stationary noise for ASR. [0.0 .. 3.0] (default: 1.1)"),
+                Self::MIN_NS_SR => ParamConfig::float(19, 37, 1., 0., Access::ReadWrite, "Gain-floor for stationary noise suppression for ASR. [-inf .. 0] dB (default: -16dB = 20log10(0.15))"),
+                Self::MIN_NN_SR => ParamConfig::float(19, 38, 1., 0., Access::ReadWrite, "Gain-floor for non-stationary noise suppression for ASR. [-inf .. 0] dB (default: -10dB = 20log10(0.3))"),
+                Self::GAMMAVAD_SR => ParamConfig::float(19, 39, 1000., 0., Access::ReadWrite, "Set the threshold for voice activity detection. [-inf .. 60] dB (default: 3.5dB 20log10(1.5))"),
+                Self::DOAANGLE => ParamConfig::int_n(21, 0, 359, 0, Access::ReadOnly, "DOA angle. Current value. Orientation depends on build configuration.", "[0 .. 359] Angle")
+            }
+        })[self.clone()]
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ParamConfig {
+    IntN(Config<i32>),
+    Int2(Config<i32>),
+    Int3(Config<i32>),
+    Int4(Config<i32>),
+    Float(Config<f32>),
+}
+
+impl ParamConfig {
+    fn int_n(
+        id: u16,
+        cmd: u16,
+        max: i32,
+        min: i32,
+        access: Access,
+        description: &str,
+        value_description_1: &str,
+    ) -> Self {
+        Self::IntN(Config {
+            id,
+            cmd,
+            min,
+            max,
+            access,
+            description: description.to_string(),
+            value_descriptions: vec![value_description_1.to_string()],
+        })
+    }
+
+    fn int2(
+        id: u16,
+        cmd: u16,
+        max: i32,
+        min: i32,
+        access: Access,
+        description: &str,
+        value_description_1: &str,
+        value_description_2: &str,
+    ) -> Self {
+        Self::Int2(Config {
+            id,
+            cmd,
+            min,
+            max,
+            access,
+            description: description.to_string(),
+            value_descriptions: vec![
+                value_description_1.to_string(),
+                value_description_2.to_string(),
+            ],
+        })
+    }
+
+    fn int3(
+        id: u16,
+        cmd: u16,
+        max: i32,
+        min: i32,
+        access: Access,
+        description: &str,
+        value_description_1: &str,
+        value_description_2: &str,
+        value_description_3: &str,
+    ) -> Self {
+        Self::Int3(Config {
+            id,
+            cmd,
+            min,
+            max,
+            access,
+            description: description.to_string(),
+            value_descriptions: vec![
+                value_description_1.to_string(),
+                value_description_2.to_string(),
+                value_description_3.to_string(),
+            ],
+        })
+    }
+
+    fn int4(
+        id: u16,
+        cmd: u16,
+        max: i32,
+        min: i32,
+        access: Access,
+        description: &str,
+        value_description_1: &str,
+        value_description_2: &str,
+        value_description_3: &str,
+        value_description_4: &str,
+    ) -> Self {
+        Self::Int4(Config {
+            id,
+            cmd,
+            min,
+            max,
+            access,
+            description: description.to_string(),
+            value_descriptions: vec![
+                value_description_1.to_string(),
+                value_description_2.to_string(),
+                value_description_3.to_string(),
+                value_description_4.to_string(),
+            ],
+        })
+    }
+
+    fn float(id: u16, cmd: u16, max: f32, min: f32, access: Access, description: &str) -> Self {
+        Self::Float(Config {
+            id,
+            cmd,
+            min,
+            max,
+            access,
+            description: description.to_string(),
+            value_descriptions: vec![],
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Config<T> {
+    pub id: u16,
+    pub cmd: u16,
+    pub min: T,
+    pub max: T,
+    pub access: Access,
+    pub description: String,
+    pub value_descriptions: Vec<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Access {
+    ReadOnly,
+    ReadWrite,
+}
