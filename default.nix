1220dcfefda2c31413db60c168a17f87d8315539
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2025-04-10 14:01:55 +0200

    fix: try to fix alsa-sys build

--- default.nix
+++ default.nix
@@ -7,5 +7,9 @@ pkgs.rustPlatform.buildRustPackage rec {
   pname = "respeaker-rs";
   version = "0.1.0";
   src = pkgs.lib.cleanSource "${theSource}";
+
+  nativeBuildInputs = [ pkgs.pkg-config ];  # Needed for build.rs of alsa-sys
+  buildInputs = [ pkgs.alsa-lib ];          # Needed so pkg-config can find alsa
+
   cargoLock.lockFile = "${theSource}/Cargo.lock";
 }
