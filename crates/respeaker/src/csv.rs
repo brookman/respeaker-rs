002231af3ff9ffe95b881e289ccccfda5b246ec1
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2025-05-27 10:05:34 +0200

    Refactor

--- crates/respeaker/src/csv.rs
+++ crates/respeaker/src/csv.rs
@@ -9,7 +9,7 @@ pub struct CsvWriter {
 }
 
 impl CsvWriter {
-    pub fn new(file_path: &PathBuf) -> eyre::Result<CsvWriter> {
+    pub fn new(file_path: &PathBuf) -> eyre::Result<Self> {
         let params: Vec<ParamKind> = ParamKind::sorted();
         let mut writer = Writer::from_writer(File::create(file_path)?);
 
@@ -18,17 +18,17 @@ impl CsvWriter {
         writer.write_record(&headers)?;
         writer.flush()?;
 
-        Ok(CsvWriter { writer })
+        Ok(Self { writer })
     }
 
-    pub fn write_row(&mut self, time: f32, row: HashMap<ParamKind, Value>) -> eyre::Result<()> {
+    pub fn write_row(&mut self, time: f32, values: &HashMap<ParamKind, Value>) -> eyre::Result<()> {
         let params: Vec<ParamKind> = ParamKind::sorted();
         let mut record = vec![time.to_string()];
 
         record.extend(
             params
                 .iter()
-                .map(|param| row.get(param).map_or_else(String::new, Value::to_string)),
+                .map(|param| values.get(param).map_or_else(String::new, Value::to_string)),
         );
 
         self.writer.write_record(&record)?;
